71a1e98674843033b12568cbc2cfa81b96c8625c
MvdS <git@vandersluys.nl>
2020-11-08 15:42:01 +0100

    Update headers (C -> Python).

--- soltrack/position.py
+++ soltrack/position.py
@@ -1,24 +1,17 @@
+#  Copyright (c) 2019-2020  Marc van der Sluys - marc.vandersluys.nl
+#   
+#  This file is part of the SolTrack Python package,
+#  see: http://soltrack.sf.net
+#   
+#  This is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#  
+#  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+#  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License along with this code.  If not, see
+#  <http://www.gnu.org/licenses/>.
 
-"""SolTrack: a simple, free, fast and accurate C routine to compute the position of the Sun.
-    
-  Copyright (c) 2014-2020 Marc van der Sluys, Paul van Kan and Jurgen Reintjes, 
-  Sustainable Energy research group, HAN University of applied sciences, Arnhem, The Netherlands
-   
-  This file is part of the SolTrack package, see: http://soltrack.sourceforge.net SolTrack is derived from
-  libTheSky (http://libthesky.sourceforge.net) under the terms of the GPL v.3
-  
-  This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
-  Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
-  option) any later version.
-  
-  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
-  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
-  License for more details.
-  
-  You should have received a copy of the GNU Lesser General Public License along with this code.  If not, see
-  <http://www.gnu.org/licenses/>.
-
-"""
 
 from dataclasses import dataclass
 import numpy as np
